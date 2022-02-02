97760c53e50e555790adb8cf04f2a945dbace9d0
sinareum <kuuniin@gmail.com>
2022-02-02 23:44:27 +0900

    백준 문제 (8진수 -> 2진수) / 8진수를 문자열로 입력

---
+++ 01월 31일/src/Baekjoon_1212_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon_1212_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String num = scanner.next();
+
+        int sum = 0;
+
+        for(int i = 0; i < num.length(); i++){
+
+        }
+    }
+}
